fb83ab3ccf888de5e2ce0d67695bcec30b944ab8
Nikolai Ng <ngnikolai16@gmail.com>
2024-07-27 19:38:12 +0800

    MediaQuery Update

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+    document.getElementById("desktop-nav").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-100px";
+    document.getElementById("desktop-nav").style.top = "-100px";
   }
   prevScrollpos = currentScrollPos;
 }
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}  
